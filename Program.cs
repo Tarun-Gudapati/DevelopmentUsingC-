6acba79e99748cf7595f959f1c3eab5707a4a8a1
Tarun Gudapati <94289102+Tarun-Gudapati@users.noreply.github.com>
2024-08-09 07:43:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Giraffe
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            String str = "Dog";
+            Console.Write("Enter a number : ");
+           int  x=Convert.ToInt32( Console.ReadLine());
+
+            Console.Write("Enter another number : ");
+            int y = Convert.ToInt32(Console.ReadLine());
+                ;
+            Console.WriteLine("the sum of the numbers is "+add(x,y));
+            Console.ReadLine();
+           
+        }
+        static int add(int x,int y)
+        {
+            return x+y;
+            
+        }
+    }
+}
